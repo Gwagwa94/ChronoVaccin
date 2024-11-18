317318852accffc6604d2edcc913ec6bf4fdcb48
Flykerz <schrivetheo@gmail.com>
2024-11-18 09:39:53 +0100

    Add CORS file

---
+++ src/main/java/com/example/vaccination/ChronoVaccinApplication.java
@@ -0,0 +1,11 @@
+package com.example.vaccination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChronoVaccinApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ChronoVaccinApplication.class, args);
+    }
+}
