1141a421b163f068898f15bd51261337f71f5824
Flykerz <schrivetheo@gmail.com>
2024-11-19 10:36:06 +0100

    recherche medecin

--- vaccination-frontend/src/app/pages/ui-components/bagde/badge.component.ts
+++ vaccination-frontend/src/app/pages/ui-components/bagde/badge.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { MedecinService, Medecin } from "../../../services/medecin.service";
+import { MedecinService, Medecin } from '../../../services/medecin.service';
 
 @Component({
   selector: 'app-badge',
@@ -7,6 +7,7 @@ import { MedecinService, Medecin } from "../../../services/medecin.service";
 })
 export class AppBadgeComponent implements OnInit {
   medecins: Medecin[] = [];
+  filteredMedecins: Medecin[] = [];
   hidden = false;
 
   constructor(private medecinService: MedecinService) {}
@@ -18,7 +19,20 @@ export class AppBadgeComponent implements OnInit {
   loadMedecins() {
     this.medecinService.getMedecins().subscribe((data) => {
       this.medecins = data;
+      this.filteredMedecins = data; // Initialisation pour afficher tous les médecins
     });
+  }
+
+  searchMedecins(event: Event): void {
+    const inputElement = event.target as HTMLInputElement; // Conversion explicite
+    const query = inputElement?.value.trim().toLowerCase(); // Accéder à la valeur
+    if (query) {
+      this.filteredMedecins = this.medecins.filter((medecin) =>
+          medecin.nom.toLowerCase().includes(query)
+      );
+    } else {
+      this.filteredMedecins = [...this.medecins]; // Réinitialiser si la recherche est vide
+    }
   }
 
   toggleBadgeVisibility() {
