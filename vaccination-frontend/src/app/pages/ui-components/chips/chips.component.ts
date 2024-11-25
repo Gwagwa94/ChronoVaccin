93981e558a60570256cd70156712792d90bb71c7
Flykerz <schrivetheo@gmail.com>
2024-11-25 10:47:52 +0100

    TODO change db

--- vaccination-frontend/src/app/pages/ui-components/chips/chips.component.ts
+++ vaccination-frontend/src/app/pages/ui-components/chips/chips.component.ts
@@ -1,37 +1,53 @@
 import { Component, OnInit } from '@angular/core';
+import { CentreService, Centre } from '../../../services/centre.service';
+import { MedecinService } from '../../../services/medecin.service';
+
 import { HttpClient } from '@angular/common/http';
-import { Centre } from '../../../services/centre.service';
-import { Medecin } from '../../../services/medecin.service';
 
 @Component({
   selector: 'app-chips',
   templateUrl: './chips.component.html',
-  styleUrls: ['./chips.component.scss'],
+  styleUrls: ['./chips.component.scss']
 })
 export class AppChipsComponent implements OnInit {
-  centres: Centre[] = [];
-  filteredCentres: Centre[] = [];
-  medecins: Medecin[] = [];
+  centres: any[] = [];
+  filteredCentres: any[] = [];
+  medecins: any[] = [];
   selectedCentreId: number | null = null;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {} // Injection ici
 
   ngOnInit(): void {
     this.loadCentres();
   }
 
   loadCentres() {
-    this.http.get<Centre[]>('http://localhost:8080/api/centers').subscribe((data) => {
+    this.http.get('http://localhost:8080/centers').subscribe((data: any) => {
       this.centres = data;
-      this.filteredCentres = [...data];
+      this.filteredCentres = [...data]; // Initialisation pour afficher tous les centres
+
+      // Charger les adresses pour chaque centre
+      this.centres.forEach((centre) => {
+        if (centre.addressId) {
+          this.loadAddress(centre.addressId, (address) => {
+            centre['address'] = address; // Ajout des détails d'adresse
+          });
+        }
+      });
+    });
+  }
+
+  loadAddress(addressId: number, callback: (address: any) => void): void {
+    this.http.get(`http://localhost:8080/address/${addressId}`).subscribe((data) => {
+      callback(data);
     });
   }
 
   loadMedecins(centreId: number) {
     this.selectedCentreId = centreId;
     this.http
-        .get<Medecin[]>(`http://localhost:8080/api/doctors?centreId=${centreId}`)
-        .subscribe((data) => {
+        .get(`http://localhost:8080/doctors?centerId=${centreId}`)
+        .subscribe((data: any) => {
           this.medecins = data;
         });
   }
@@ -41,8 +57,7 @@ export class AppChipsComponent implements OnInit {
     this.filteredCentres = this.centres.filter(
         (centre) =>
             centre.name.toLowerCase().includes(lowerQuery) ||
-            centre.address.city.toLowerCase().includes(lowerQuery) ||
-            centre.address.postalCode.includes(query)
+            (centre.address?.city && centre.address.city.toLowerCase().includes(lowerQuery))
     );
   }
 
